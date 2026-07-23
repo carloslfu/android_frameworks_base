dbaf26beed3467299b72d4f37c4a313bd9936bf4
Carlos Galarza <carloslfu@gmail.com>
2026-07-23 11:18:17 -0500

    Share package permission evaluation with refresh service

--- services/permission/java/com/android/server/permission/access/permission/AppIdPermissionPolicy.kt
+++ services/permission/java/com/android/server/permission/access/permission/AppIdPermissionPolicy.kt
@@ -934,7 +934,7 @@ class AppIdPermissionPolicy : SchemePolicy() {
         }
     }
 
-    private fun MutateStateScope.evaluateAllPermissionStatesForPackageAndUser(
+    fun MutateStateScope.evaluateAllPermissionStatesForPackageAndUser(
         packageState: PackageState,
         userId: Int,
         installedPackageState: PackageState?,
